487b665197f3caa13dd15f7d977fdb4e5f4eb62f
wadhwarohan <128384989+wadhwarohan@users.noreply.github.com>
2023-06-26 21:26:41 +0530

    Add files via upload

--- ZigZagArray.java
+++ ZigZagArray.java
@@ -1,19 +1,27 @@
-import java.util.Arrays;
-
-class ZigZagArray {
-    static int arr[] = new int[] { 3, 9, 6, 1, 7, 5, 8 };
-
-    static void zigZag() {
-        Arrays.sort(arr);
-        for (int i = 1; i <= arr.length - 2; i += 2) {
-            int temp = arr[i];
-            arr[i] = arr[i + 1];
-            arr[i + 1] = temp;
-        }
-    }
-
+public class ZigZagArray {
     public static void main(String[] args) {
-        zigZag();
-        System.out.println(Arrays.toString(arr));
+        int arr[] = { 1, 6, 9, 8, 7, 3, 5 };
+        boolean flag = true;
+        int temp = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (flag) {
+                if (arr[i] > arr[i + 1]) {
+                    temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
+            } else {
+                if (arr[i] < arr[i + 1]) {
+                    temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
+            }
+            flag = !flag;
+        }
+        System.out.println("Zig Zag Array...");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
     }
 }
